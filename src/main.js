5a5a87c69d05b799ecf359c8727d952010ec430f
Denis <dkovalevskiy02@gmail.com>
2025-04-05 17:57:26 +0300

    added axios and izitoast

--- src/main.js
+++ src/main.js
@@ -3,3 +3,6 @@ import Accordion from "accordion-js";
 import "accordion-js/dist/accordion.min.css";
 import Swiper from 'swiper';
 import 'swiper/css';
+import axios from 'axios';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
