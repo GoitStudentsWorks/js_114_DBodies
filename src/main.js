8fad6beb1005cb1e9cf83bf131eddc10e68c3be6
Yurii Pasternak <yuriipasternak@icloud.com>
2025-04-05 23:02:54 +0200

    js-project-mobil

--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
 import 'modern-normalize/modern-normalize.css';
-import Accordion from "accordion-js";
-import "accordion-js/dist/accordion.min.css";
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
 import Swiper from 'swiper';
 import 'swiper/css';
+
+const form = document.getElementById('contactForm');
+const modal = document.getElementById('modal');
+const closeModal = document.getElementById('closeModal');
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  modal.style.display = 'flex';
+  form.reset();
+});
+
+closeModal.addEventListener('click', function () {
+  modal.style.display = 'none';
+});
+
+window.addEventListener('click', function (e) {
+  if (e.target === modal) {
+    modal.style.display = 'none';
+  }
+});
