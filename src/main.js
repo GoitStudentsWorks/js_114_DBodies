b448efd28f1fc3bc108fac7ab1edec3ccbd993e4
Denis <dkovalevskiy02@gmail.com>
2025-04-06 11:50:34 +0300

    hidden swiper.css

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import 'modern-normalize/modern-normalize.css';
 import Accordion from "accordion-js";
 // import "accordion-js/dist/accordion.min.css";
 import Swiper from 'swiper';
-import 'swiper/css';
+// import 'swiper/css';
 import axios from 'axios';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
