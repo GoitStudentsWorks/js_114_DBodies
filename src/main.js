28a6823f714db34d042cdd6a6f09f6eafc92c333
Denis <dkovalevskiy02@gmail.com>
2025-04-06 11:21:32 +0300

    hidden css accordeon

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import 'modern-normalize/modern-normalize.css';
 import Accordion from "accordion-js";
-import "accordion-js/dist/accordion.min.css";
+// import "accordion-js/dist/accordion.min.css";
 import Swiper from 'swiper';
 import 'swiper/css';
 import axios from 'axios';
