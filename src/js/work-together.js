89d3fc20ae5d4207fa6c87a0a9e7a20f0f020f0e
Denis <dkovalevskiy02@gmail.com>
2025-04-08 12:35:34 +0300

    added container for footer

--- src/js/work-together.js
+++ src/js/work-together.js
@@ -1,8 +1,3 @@
-import 'modern-normalize/modern-normalize.css';
-import Accordion from 'accordion-js';
-import 'accordion-js/dist/accordion.min.css';
-import Swiper from 'swiper';
-import 'swiper/css';
 
 const modal = document.getElementById('thankYouModal');
 const closeBtn = document.getElementById('closeModal');
